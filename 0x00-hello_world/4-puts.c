86db311cf7d8e34c9363cc250f9b35e68737af77
Prince Senanu Dickson <freegiver2012@gmail.com>
2023-02-17 07:20:47 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #!/bin/bash
 #include <stdio.h>
-
 /**
  * main - entry point
  * return: always (0)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle);
-	return(0);
+	return 0;
 }
