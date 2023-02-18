c584307565e8e3219c831b8cebe6581c197806f8
FreeGiver <freegiver2012@gmail.com>
2023-02-18 03:34:11 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("size of a char: %zu byete(s)\n"), sizeof(char));
-	pribtf("size of an int: %zu byete(s)\n"), sizeof(int));
-	printf("size of a long int: %zu byete(s)\n"), sizeof(long int));
-	printf("size of a long long int: %zu byete(s)\n"), sizeof(long long int));
-	printf("size of a float: %zu byete(s)\n"), sizeof(float));
+	printf("size of a char: %zu byete(s)\n"), sizeof(char);
+	pribtf("size of an int: %zu byete(s)\n"), sizeof(int);
+	printf("size of a long int: %zu byete(s)\n"), sizeof(long int);
+	printf("size of a long long int: %zu byete(s)\n"), sizeof(long long int);
+	printf("size of a float: %zu byete(s)\n"), sizeof(float);
 	return (0);
 }
