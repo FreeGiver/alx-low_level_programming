43a2036dc375b42aadd8456443034a86684a9a97
FreeGiver <freegiver2012@gmail.com>
2023-02-21 15:21:12 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * _putchar - prints the characters (_putcher)
+ * return: 0 (success)
+ */
+int (main)
+{
+	_putcher("_");
+	_putcher("p");
+	_putcher("u");
+	_putcher("t");
+	_putcher("c");
+	_putcher("h");
+	_putcher("e");
+	_putcher("r");
+	_putcher('\n');
+	return(0);
+}
