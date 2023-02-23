a9e511dc98dbb45304b1d633b7933bc9c4ef5093
FreeGiver <freegiver2012@gmail.com>
2023-02-22 20:01:56 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,17 +5,19 @@
  * print_alphabet_x10 - prints all alphabets in lowercase 10 times.
  *
  * Return: On success 1
+ *
  */
+
 void print_alphabet_x10(void)
 {
-	char i, k;
+	char p, k;
 
 	for (k = 0; k < 10; k++)
 	{
 
-		for (i = 'a'; i <= 'z'; i++)
+		for (p = 'a'; p <= 'z'; i++)
 		{
-			_putchar(i);
+			_putchar(p);
 		}
 		_putchar('\n');
 	}
